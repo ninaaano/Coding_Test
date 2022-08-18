fd7a5efb2ebe15567353ac9edfd47230a19dc5ad
Life is a ninanino <95615105+ninaaano@users.noreply.github.com>
2022-08-18 16:30:10 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.02 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(int num) {
+
+        String even = "Even";
+        String odd = "Odd";
+        
+        if(num%2==0){
+            return even;
+        } else
+            return odd;
+      
+    }
+}
