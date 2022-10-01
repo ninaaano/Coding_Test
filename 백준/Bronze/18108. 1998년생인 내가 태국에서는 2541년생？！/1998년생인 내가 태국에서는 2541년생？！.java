e5ce2cadcb81f98557eec06ed0ed916ffab442e7
Life is a ninanino <95615105+ninaaano@users.noreply.github.com>
2022-10-01 16:21:24 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 208 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int y = a-543;
+        System.out.println(y);       
+    }
+}
