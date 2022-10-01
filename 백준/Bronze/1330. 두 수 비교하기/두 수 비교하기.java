d2d41540196f867c24e20f8786f79442e82cfbc3
Life is a ninanino <95615105+ninaaano@users.noreply.github.com>
2022-10-01 21:11:36 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 124 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -1,22 +1,14 @@
-/*
-A가 B보다 큰 경우에는 '>'를 출력한다.
-A가 B보다 작은 경우에는 '<'를 출력한다.
-A와 B가 같은 경우에는 '=='를 출력한다.
-*/
 import java.util.*;
+import java.io.*;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        
-        if(a>b){
-            System.out.println(">");
-        }else if(a<b){
-            System.out.println("<");
-        }else{
-            System.out.println("==");
-        }
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String[] str = br.readLine().split(" ");
+		int A = Integer.parseInt(str[0]);
+		int B = Integer.parseInt(str[1]);
+
+		System.out.println((A>B) ? ">" : ((A<B) ? "<" : "==" ));
     }
 }
