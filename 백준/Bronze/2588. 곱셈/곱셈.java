74dd1e0ad3b921d443d788f7e68f77a67a5cf39c
Life is a ninanino <95615105+ninaaano@users.noreply.github.com>
2022-10-01 19:02:05 +0900

    [Bronze III] Title: 곱셈, Time: 124 ms, Memory: 14048 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        String b = br.readLine();
+
+        br.close();
+
+        System.out.println(a*(b.charAt(2)-'0'));
+        System.out.println(a*(b.charAt(1)-'0'));
+        System.out.println(a*(b.charAt(0)-'0'));
+        System.out.println(a*Integer.parseInt(b));
+
+    }
+}
