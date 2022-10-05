c30671acc7c1a42a9faca1a7689324cc212d117c
Life is a ninanino <95615105+ninaaano@users.noreply.github.com>
2022-10-05 19:48:46 +0900

    [Bronze V] Title: A+B - 7, Time: 156 ms, Memory: 16276 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -6,14 +6,12 @@ public class Main{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
 
         for(int i = 0; i<n; i++){
             StringTokenizer st = new StringTokenizer(br.readLine()+" ");
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            arr[i] = a+b;
-            bw.write("Case #"+String.valueOf(i+1)+": "+String.valueOf(arr[i]));
+            bw.write("Case #"+String.valueOf(i+1)+": "+String.valueOf(a+b));
             bw.newLine();
         }
 
