1e4306e74f597e48f2cb439cf13ac0710036c834
Life is a ninanino <95615105+ninaaano@users.noreply.github.com>
2022-10-05 19:55:47 +0900

    [Bronze V] Title: A+B - 8, Time: 180 ms, Memory: 16560 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine() + " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            bw.write("Case #" + String.valueOf(i + 1) + ": " +String.valueOf(a)+" + "+String.valueOf(b)+" = "+ String.valueOf(a + b));
+            bw.newLine();
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
