ac22b3d9102ab1c1f57ca09ecfa564164c8d5bd3
Life is a ninanino <95615105+ninaaano@users.noreply.github.com>
2022-10-31 18:04:19 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 140 ms, Memory: 14340 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i=1; i<=n; i++){
+            for(int k=1; k<i; k++){
+                bw.write(" ");
+            }
+            for(int j=1; j<=2*(n-i)+1; j++) {
+                bw.write("*");
+            }
+
+            bw.newLine();
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
