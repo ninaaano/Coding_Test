1a5768e3b288ffd3dc31001bae764ae3877f90b4
Life is ninanino <tvxqinak@naver.com>
2023-01-08 20:46:49 +0900

    [Bronze II] Title: 수 정렬하기, Time: 184 ms, Memory: 15320 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,17 +1,31 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        /**
+         * 버블정렬을 사용해서 풀어보기
+         */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
-        for(int i = 0; i<n; i++){
-            arr[i] = sc.nextInt();
-        }
-        Arrays.sort(arr);
+
         for(int i=0; i<n; i++){
-            System.out.println(arr[i]);
+            arr[i] = Integer.parseInt(br.readLine());
         }
+        for(int i=0; i<n-1; i++){
+            for(int j=0; j<n-1-i; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        for(int x : arr) {
+            System.out.println(x);
+        }
+        br.close();
     }
 }
