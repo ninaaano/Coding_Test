60fd4fca83fe8a21cb38fad36600ad8691e564f2
Life is ninanino <tvxqinak@naver.com>
2023-01-18 19:40:47 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 696 ms, Memory: 141404 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -5,24 +5,24 @@ import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
-    static boolean[] visited; // 방문 배열
-    static ArrayList<Integer>[] A; // 인접 리스트
+    static boolean[] visited;
+    static ArrayList<Integer>[] A;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        visited = new boolean[n+1];
-        A = new ArrayList[n+1]; // 실제로 모든 객체를 생성한 건 아님. 시작점만 있음
+        visited = new boolean[n + 1];
+        A = new ArrayList[n + 1];
 
         for(int i=1; i<=n; i++){
             A[i] = new ArrayList<>();
         }
 
-        // 엣지 갯수 만큼 데이터 저장하기
-        for(int i=0; i<m; i++){
+        for (int i =0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
@@ -32,8 +32,8 @@ public class Main {
 
         int count = 0;
 
-        for(int i=1; i<=n; i++){
-            if(!visited[i]){
+        for (int i = 1; i <= n; i++) {
+            if (!visited[i]) {
                 count++;
                 DFS(i);
             }
@@ -41,12 +41,12 @@ public class Main {
         System.out.println(count);
     }
 
-    private static void DFS(int i) {
-        if(visited[i]) return;
-        visited[i] = true;
-        for(int x : A[i]){
-            if(!visited[x]) {
-                DFS(x);
+    private static void DFS(int x) {
+        if (visited[x]) return;
+        visited[x] = true;
+        for (int e : A[x]) {
+            if (!visited[e]) {
+                DFS(e);
             }
         }
     }
