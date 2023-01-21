d26ca6173f234dbe0f14f10083bb340987c25d4b
Life is ninanino <tvxqinak@naver.com>
2023-01-21 21:33:21 +0900

    [Silver I] Title: 미로 탐색, Time: 132 ms, Memory: 14656 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[][] arr;
+    static boolean[][] visited;
+    static int n,m;
+    static int[] dx = {-1,1,0,0}; // x방항배열 - 상하
+    static int[] dy = {0,0,-1,1}; // y방향배열 - 좌우
+    static Queue<int[]> queue;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken()); // 시작
+        m = Integer.parseInt(st.nextToken()); // 끝
+
+        arr = new int[n][m];
+
+        for(int i=0; i<n; i++){
+            String input = br.readLine();
+            for(int j=0; j<m; j++){
+                arr[i][j] = input.charAt(j) - '0';
+            }
+        }
+
+        visited = new boolean[n][m];
+        visited[0][0] = true;
+        bfs(0,0);
+        System.out.println(arr[n-1][m-1]);
+    }
+
+    private static void bfs(int x, int y) {
+        queue = new LinkedList<>();
+        queue.add(new int[] {x,y});
+
+        while (!queue.isEmpty()){
+           int now[] = queue.poll();
+           int nowX = now[0];
+           int nowY = now[1];
+
+            for(int i = 0; i<4; i++){
+                int nextX = nowX + dx[i];
+                int nextY = nowY + dy[i];
+
+                if(nextX < 0 || nextY < 0 || nextX >= n || nextY >= m){
+                    continue;
+                }
+
+                if(visited[nextX][nextY] || arr[nextX][nextY] == 0) {
+                    continue;
+                }
+                queue.add(new int[] {nextX,nextY});
+                arr[nextX][nextY] = arr[nowX][nowY] + 1;
+                visited[nextX][nextY] = true;
+            }
+        }
+
+    }
+}
