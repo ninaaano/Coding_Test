ba1084c87ed28469f75e9a606c4f80fb33700de3
Life is ninanino <tvxqinak@naver.com>
2023-01-29 01:14:23 +0900

    [Silver I] Title: 회의실 배정, Time: 532 ms, Memory: 43252 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -1,54 +1,60 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
-/**
- * 회의실 배정
- * 회의가 끝나는 시간을 기준으로 오름차순 정렬
- * 회의가 먼저 시작하는 걸 선택하면 중간에 시작하는 회의를 선택할 수 없다
- * 만약 끝나는 시간이 같다면 시작 시간을 오름차순 정렬 해야한다
- */
+class Node implements Comparable<Node>{
+    int start;
+    int end;
 
-class Time implements Comparable<Time> {
-
-    public int start, end;
-
-    public Time(int start, int end) {
+    public Node(int start, int end) {
         this.start = start;
         this.end = end;
     }
 
     @Override
-    public int compareTo(Time o) {
-        if (this.end == o.end) return this.start - o.start; // 오름차순
-        else return this.end - o.end;
+    public int compareTo(Node o) {
+        // 끝나는 시간이 같다면 시작 시간 기준으로 오름차순
+        if(this.end == o.end) {
+            return this.start - o.start;
+        }else
+            return this.end - o.end;
     }
 }
 
 public class Main {
-    public static void main(String[] args) {
-        Main T = new Main();
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        ArrayList<Time> procedure = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            int start = sc.nextInt();
-            int end = sc.nextInt();
-            procedure.add(new Time(start, end));
-        }
-        System.out.println(T.solution(n, procedure));
-    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    private int solution(int n, ArrayList<Time> procedure) {
+        int n = Integer.parseInt(br.readLine());
+
+        ArrayList<Node> time = new ArrayList<>();
+
+        StringTokenizer st;
+
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            time.add(new Node(s,e));
+        }
+
+        Collections.sort(time);
+
         int answer = 0;
-        Collections.sort(procedure); // compareTo 기준에 의해 정렬
-        int et = 0; // 현재 회의의 끝나는 시간
-        for (Time object : procedure) {
-            if (object.start >= et) { // 회의 가능
+        int prev_et = 0;
+
+        for(Node i : time){
+            if(i.start >= prev_et){
                 answer++;
-                et = object.end;
+                prev_et = i.end;
             }
         }
-        return answer;
+
+        System.out.println(answer);
+
     }
 }
