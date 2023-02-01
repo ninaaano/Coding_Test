4414849fc3617cd581e7cb41b35d2aaa439a47c4
Life is ninanino <tvxqinak@naver.com>
2023-02-01 19:30:55 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 372 ms, Memory: 25948 KB -BaekjoonHub

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
@@ -1,29 +1,26 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+public class Main{
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-
+        
         int n = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < n; i++) {
+        
+        for(int i=0; i<n; i++){
             pq.add(Integer.parseInt(br.readLine()));
         }
+        
         int sum = 0;
-
-        while(pq.size() > 1){
+        
+        while(pq.size()>1){
             int a = pq.poll();
             int b = pq.poll();
-            sum += a+b;
+            sum += (a+b);
             pq.add(a+b);
         }
-
+        
         System.out.println(sum);
     }
-
 }
