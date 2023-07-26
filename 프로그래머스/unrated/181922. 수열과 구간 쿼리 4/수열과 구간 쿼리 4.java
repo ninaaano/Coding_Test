1617b3281e7768820a8165c59dc962a072dae28e
Life is ninanino <tvxqinak@naver.com>
2023-07-26 15:30:27 +0900

    [unrated] Title: 수열과 구간 쿼리 4, Time: 1.32 ms, Memory: 80.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = Arrays.copyOf(arr, arr.length);
+        for(int[] query : queries) {
+            int s = query[0];
+            int e = query[1];
+            int k = query[2];
+            
+            for(int i=s; i<=e; i++) {
+                if(i % k == 0) {
+                    answer[i]++;
+                }
+            }
+        }
+        return answer;
+    }
+}
