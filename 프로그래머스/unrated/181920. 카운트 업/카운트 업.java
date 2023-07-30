578352d214b67d47280b7fb012928f31ba1e4255
Life is ninanino <tvxqinak@naver.com>
2023-07-30 23:31:23 +0900

    [unrated] Title: 카운트 업, Time: 0.01 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
@@ -1,5 +1,7 @@
 class Solution {
     public int[] solution(int start, int end) {
+        // 아래와 같은 방법도 있다
+        // IntStream.rangeClosed(start, end).toArray();
         int[] answer = new int[end-start+1];
         for(int i=0; i<answer.length; i++) {
             answer[i] = start + i;
