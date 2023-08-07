3a894ad276f2886be6962674a6ef70a7057a468b
Life is ninanino <tvxqinak@naver.com>
2023-08-07 12:45:17 +0900

    [unrated] Title: 접미사 배열, Time: 0.25 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    public String[] solution(String my_string) {
+    
+        int n = my_string.length();
+        String[] answer = new String[n];
+        
+        for(int i=0; i<answer.length; i++) {
+            answer[i] = my_string.substring(i);
+        }
+        
+        Arrays.sort(answer);
+        
+        return answer;
+    }
+}
