562497725b706d1ce526b695ff76e7a564fb8d77
Life is ninanino <tvxqinak@naver.com>
2023-08-11 22:45:00 +0900

    [unrated] Title: qr code, Time: 8.73 ms, Memory: 68 MB -BaekjoonHub

--- 프로그래머스/unrated/181903. qr code/qr code.java
+++ 프로그래머스/unrated/181903. qr code/qr code.java
@@ -1,11 +1,11 @@
 class Solution {
     public String solution(int q, int r, String code) {
-        StringBuilder answer = new StringBuilder();
-
-        for (int i = r; i < code.length(); i += q) {
-            answer.append(code.charAt(i));
+        String answer = "";
+        for(int i=0; i<code.length(); i++) {
+            if(i % q == r) {
+                answer += code.charAt(i);
+            }
         }
-
-        return answer.toString();
+        return answer;
     }
 }
