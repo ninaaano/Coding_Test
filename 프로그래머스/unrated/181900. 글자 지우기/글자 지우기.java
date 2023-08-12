6bd404b9dac65ca1b18f9a27fd8a1aa1b508bfa5
Life is ninanino <tvxqinak@naver.com>
2023-08-13 00:33:21 +0900

    [unrated] Title: 글자 지우기, Time: 5.16 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.java
@@ -5,7 +5,8 @@ import java.util.stream.IntStream;
 class Solution {
     public String solution(String my_string, int[] indices) {
         String answer = IntStream.range(0, my_string.length())
-                .filter(i -> !Arrays.stream(indices).anyMatch(index -> index == i))
+                .filter(i -> !Arrays.stream(indices)
+                .anyMatch(index -> index == i))
                 .mapToObj(my_string::charAt)
                 .map(String::valueOf)
                 .collect(Collectors.joining());
